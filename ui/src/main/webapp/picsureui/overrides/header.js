bcae3f1136b048bdb0d55fdacd94866fdfa70ac4
Nate Chu <chu.nathan@gmail.com>
2021-12-15 16:15:43 -0500

    load dataInfo module

--- ui/src/main/webapp/picsureui/overrides/header.js
+++ ui/src/main/webapp/picsureui/overrides/header.js
@@ -1,4 +1,7 @@
-define([], function(){
+define(["data/dataInfo"], function(dataInfo){
+	
+	// Don't need to do anything with data info, just load it.
+	
 	return {
 		/*
 		 * The path to a logo image incase you don't want the default PrecisionLink one.
