8de15e6c5c65d219837cc0eaf72854b6f1fd31ce
Nate Chu <chu.nathan@gmail.com>
2022-01-03 13:49:58 -0500

    use new header render override hook

--- ui/src/main/webapp/picsureui/overrides/header.js
+++ ui/src/main/webapp/picsureui/overrides/header.js
@@ -1,10 +1,14 @@
 define(["jquery", "backbone", "data/dataInfo"], function($, BB, dataInfo){
 	
-	dataInfo = new dataInfo.View(this.resources);
-	dataInfo.setElement($("#data-info-btn"));
-	dataInfo.render();
 	
 	return {
+		
+		dataInfo:  new dataInfo.View(this.resources),
+		
+		render: function(){
+			dataInfo.setElement($("#data-info-btn"));
+			dataInfo.render();
+		},
 		/*
 		 * The path to a logo image incase you don't want the default PrecisionLink one.
 		 * 
