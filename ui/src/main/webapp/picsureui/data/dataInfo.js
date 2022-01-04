f237bf90ddc7d5da6fcb0e9d3708fc9ad546cb50
Nate Chu <chu.nathan@gmail.com>
2022-01-04 13:45:19 -0500

    use events hash for consistency

--- ui/src/main/webapp/picsureui/data/dataInfo.js
+++ ui/src/main/webapp/picsureui/data/dataInfo.js
@@ -9,9 +9,57 @@ define(["jquery", "backbone", "handlebars", "text!data/dataInfo.hbs", "text!data
                 this.modalTemplate = HBS.compile(modalTemplate);
             },
             events: {
-                "click #data-info-btn" : "showDataInfo"
+                "click #data-info-btn" : "showDataInfo",
+                "click #data-info-form-btn" : "showDataForm",
+                "click #data-info-submit-btn" : "submitDataForm"
             },
-            showDataInfo: function(event){
+            
+            showDataForm: function(){
+            	resouce =  this.resources.filter(x => x.name == this.managedSite)[0];
+            	$(".modal-body").html(this.dataInfoFormTemplate(resouce));
+            	delegateEvents();
+            },
+            submitDataForm: function(){
+            	resouce =  this.resources.filter(x => x.name == this.managedSite)[0];
+            	
+            	metadata = {};
+				metadata.clinicalPopulation = $("#clinicalPopulation").val();
+				metadata.genomicPopulation = $("#genomicPopulation").val();
+				metadata.genomicDataDiagnosis = $("#genomicDataDiagnosis").val();
+				metadata.genomicDataTypes = $("#genomicDataTypes").val();
+				metadata.biosamplePopulation = $("#biosamplePopulation").val();
+				metadata.firstDataPoint = $("#firstDataPoint").val();
+				metadata.clinicalDate = $("#clinicalDate").val();
+				metadata.genomicDate = $("#genomicDate").val();
+				metadata.availability = $("#availability").val();
+            	
+				resource.metadata = JSON.stringify(metadata);
+				
+				resourceList = [resource];
+				
+				$.ajax({
+					url: window.location.origin + '/picsure/resource',
+					headers: {"Authorization": "Bearer " + session.token},
+					contentType: 'application/json',
+					type:'PUT',
+					data: JSON.stringify(resourceList),
+					success: function(){
+			    		
+						//update stored resource
+						session = JSON.parse(sessionStorage.getItem("session"));
+						resouce =  session.resources.filter(x => x.name == this.managedSite)[0];
+						resource.metadata = metadata;
+						sessionStorage.setItem("session", JSON.stringify(session));
+						
+						alert("success");				
+					},
+					error: function(response){
+						console.log("unable to update resource: " + response.responseText);
+					}
+				});
+				
+            },
+            showDataInfo: function(){
             	
         		session = JSON.parse(sessionStorage.getItem("session"));
         		
@@ -31,54 +79,9 @@ define(["jquery", "backbone", "handlebars", "text!data/dataInfo.hbs", "text!data
                     $("#modalDialog").hide();
                 });
 
-                $("#data-info-form-btn").click(function(){
-                	resouce =  this.resources.filter(x => x.name == this.managedSite)[0];
-                	 $(".modal-body").html(this.dataInfoFormTemplate(resouce));
-                }.bind(this));
-                
-                $("#data-info-submit-btn").click(function(){
-                	resouce =  this.resources.filter(x => x.name == this.managedSite)[0];
-                	
-                	metadata = {};
-    				metadata.clinicalPopulation = $("#clinicalPopulation").val();
-    				metadata.genomicPopulation = $("#genomicPopulation").val();
-    				metadata.genomicDataDiagnosis = $("#genomicDataDiagnosis").val();
-    				metadata.genomicDataTypes = $("#genomicDataTypes").val();
-    				metadata.biosamplePopulation = $("#biosamplePopulation").val();
-    				metadata.firstDataPoint = $("#firstDataPoint").val();
-    				metadata.clinicalDate = $("#clinicalDate").val();
-    				metadata.genomicDate = $("#genomicDate").val();
-    				metadata.availability = $("#availability").val();
-                	
-    				resource.metadata = JSON.stringify(metadata);
-    				
-    				resourceList = [resource];
-    				
-    				$.ajax({
-    					url: window.location.origin + '/picsure/resource',
-    					headers: {"Authorization": "Bearer " + session.token},
-    					contentType: 'application/json',
-    					type:'PUT',
-    					data: JSON.stringify(resourceList),
-    					success: function(){
-				    		
-    						//update stored resource
-    						session = JSON.parse(sessionStorage.getItem("session"));
-    						resouce =  session.resources.filter(x => x.name == this.managedSite)[0];
-    						resource.metadata = metadata;
-    						sessionStorage.setItem("session", JSON.stringify(session));
-    						
-    						alert("success");				
-    					},
-    					error: function(response){
-    						console.log("unable to update resource: " + response.responseText);
-    					}
-    				});
-    				
-                }.bind(this));
-                
-                
                 $("#modalDialog").show();
+                
+                delegateEvents();
             }
         });
         
