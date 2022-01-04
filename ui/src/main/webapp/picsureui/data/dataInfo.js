b81ab17d34c22e43078ee6feda982cf506654418
Nate Chu <chu.nathan@gmail.com>
2022-01-03 20:23:28 -0500

    clean up & rework input form

--- ui/src/main/webapp/picsureui/data/dataInfo.js
+++ ui/src/main/webapp/picsureui/data/dataInfo.js
@@ -13,10 +13,15 @@ define(["jquery", "backbone", "handlebars", "text!data/dataInfo.hbs", "text!data
             },
             showDataInfo: function(event){
             	
-            	if(!this.resources){
-            		session = JSON.parse(sessionStorage.getItem("session"));
-            		this.resources = session.resources;
-            	}
+        		session = JSON.parse(sessionStorage.getItem("session"));
+        		
+        		//attache resources to session object for use in template
+        		this.resources = session.resources;
+        		
+        		managerPrivs = sessionprivileges.filter(x => x.includes("DATA_MANAGER_"))
+        		if(managerPrivs){
+        			siteName = managerPrivs[0].substring("DATA_MANAGER_".length);
+        		}
             	
                 $("#modal-window").html(this.modalTemplate({title: "Institution Data Information"}));
                 $(".modal-body").html(this.dataInfoTemplate(this));
@@ -26,8 +31,10 @@ define(["jquery", "backbone", "handlebars", "text!data/dataInfo.hbs", "text!data
                 });
 
                 $("#data-info-form-btn").click(function(){
+                	JSON.parse(sessionStorage.getItem("session")).privileges;
+                	
                 	 $(".modal-body").html(this.dataInfoFormTemplate(this));
-                });
+                }.bind(this));
                 
                 $("#modalDialog").show();
             }
