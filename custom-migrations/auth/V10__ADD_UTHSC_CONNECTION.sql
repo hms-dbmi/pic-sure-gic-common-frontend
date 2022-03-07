23c4c218fe78ce3da129d0877eaad8e04f3d755a
Nathan Chu <chu.nathan@gmail.com>
2022-03-07 11:36:35 -0500

    Add UTHSC button to login

---
+++ custom-migrations/auth/V10__ADD_UTHSC_CONNECTION.sql
@@ -0,0 +1,1 @@
+INSERT INTO `connection` VALUES (0xB6BD9D7F9E3311ECA5F4126ACB86EEFB, 'UTHSC', 'uthsc','samlp|UTHSC|','[{"label":"The University of Tennessee Health Science Center Email", "id":"email"}]');
