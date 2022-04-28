86fa127b4665690a86a43f93cceb5552a9608f15
Nathan Chu <chu.nathan@gmail.com>
2022-04-28 11:52:01 -0400

    add migration to fix UTHSC config

---
+++ custom-migrations/auth/V10.1__UPDATE_UTHSC_USER_METADATA_MAPPING.sql
@@ -0,0 +1,4 @@
+INSERT INTO `userMetadataMapping` VALUES 
+(unhex('B6BD9D7F9E3311ECA5F4126ACB86EEFB'), '$.email', unhex('97FD002DC366B0D8420F998F885D0ED7'), '$.email');
+
+update connection set id = 'UTHSC' where id = 'uthsc';
