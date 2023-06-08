bc8cc1b48aaa505009d8904f6aede38116f0d7be
James <Jamestp19@gmail.com>
2023-06-08 17:25:44 -0400

    Fix loops to use the object

--- ui/src/main/webapp/picsureui/overrides/outputPanel.js
+++ ui/src/main/webapp/picsureui/overrides/outputPanel.js
@@ -403,16 +403,16 @@ const biosampleColumns = [
 function createDatatable(resources, sampleFields, isGenomic) {
 	let sampleTableData = [];
 
-	resources.forEach((resource) => {
+	Object.keys(resources).forEach((resource) => {
 		let row = {};
-		row.site = resource.name;
-		row.patientCount = isGenomic ? resource.genomicdataCount : resource.biosampleCount;
+		row.site = resources[resource].name;
+		row.patientCount = isGenomic ? resources[resource].genomicdataCount : resources[resource].biosampleCount;
 
 		sampleFields.forEach(sampleField => {
 			if (isGenomic) {
-				row[sampleField.id] = resource.genomicdataCounts[sampleField.id];
-			} else if (resource.bioSampleCounts[sampleField.id]) {
-				row[sampleField.id] = resource.bioSampleCounts[sampleField.id];
+				row[sampleField.id] = resources[resource].genomicdataCounts[sampleField.id];
+			} else if (resources[resource].bioSampleCounts[sampleField.id]) {
+				row[sampleField.id] = resources[resource].bioSampleCounts[sampleField.id];
 			}
 		});
 
