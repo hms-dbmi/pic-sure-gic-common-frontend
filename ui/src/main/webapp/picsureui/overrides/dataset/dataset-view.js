49dcf6a46d162af453c227c356484389460f19de
Samantha Piatt <samantha.piatt@childrens.harvard.edu>
2023-11-02 13:30:51 -0400

    Fix spacing.

--- ui/src/main/webapp/picsureui/overrides/dataset/dataset-view.js
+++ ui/src/main/webapp/picsureui/overrides/dataset/dataset-view.js
@@ -3,22 +3,22 @@ define([ "jquery" ], function($) {
         mappers: function(map){
             return {
                 ...map,
-				uuid: {
-					path: ['metadata', 'siteQueryIds'],
-					renderId: "detail-summary-id",
-					render: function(siteList){
-						return siteList.map(({ name, queryId }) => 
-							`<div id="site-${name}" class="row p-0">` +
-								`<div class="col-md-3 p-0">${name}</div>` +
-								`<div class="col-md-9 p-0">${queryId}</div>` +
-							`</div>`
-						).join('');
-					}
-				}
-			};
-		},
-		renderExt: function(){
-			$("#cancel-btn").removeClass("secondary");
-		}
+                uuid: {
+                    path: ['metadata', 'siteQueryIds'],
+                    renderId: "detail-summary-id",
+                    render: function(siteList){
+                        return siteList.map(({ name, queryId }) => 
+                            `<div id="site-${name}" class="row p-0">` +
+                                `<div class="col-md-3 p-0">${name}</div>` +
+                                `<div class="col-md-9 p-0">${queryId}</div>` +
+                            `</div>`
+                        ).join('');
+                    }
+                }
+            };
+        },
+        renderExt: function(){
+            $("#cancel-btn").removeClass("secondary");
+        }
     };
 });
