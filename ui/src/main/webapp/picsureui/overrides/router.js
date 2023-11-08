90f95dfd7a6fa80674c62a089814c36d1f0844de
James <Jamestp19@gmail.com>
2023-11-08 09:03:26 -0500

    GIC tour overrides

--- ui/src/main/webapp/picsureui/overrides/router.js
+++ ui/src/main/webapp/picsureui/overrides/router.js
@@ -17,6 +17,17 @@ define(['help/help-page'], function(helpPage){
                 $("#help-page-btn").addClass('active');
             }
         },
+        tourMessages: [
+            '<h4><strong class="color-tertiary">GIC</strong> Portal at a Glance:</h4>',
+            '<li class="listless-li"><strong class="color-tertiary">Explore</strong> clinical, genomic, and biosample <strong>data</strong> from broadly <strong>consented</strong> cohorts, across multiple sites of care</li>',
+            '<li class="listless-li"><strong class="color-tertiary">Query</strong> combined  phenotypic/genomic queries in <strong>real-time</strong></li>',
+            '<li class="listless-li"><strong class="color-tertiary">Build cohorts</strong> and scout hypotheses to study <strong>prior</strong> to requesting data</li>',
+            '<li class="listless-li"><strong class="color-tertiary">Request access</strong> to <strong>patient-level data</strong> by submitting the Samples & Data Request form</li>',
+            '<li class="listless-li"><strong class="color-tertiary">Create & export</strong> datasets with select variables of interest to our <strong>secure</strong> research <strong>analysis</strong> platform - Service Workbench</li>',
+            '<hr>',
+            'During the tour, <strong>click anywhere</strong> to advance. Press <strong>escape</strong> to exit the tour at any point.'
+        ],
+        tourTitle: 'Welcome to the GIC',
         /**
          * Allows the default action to be overriden. If this is not set, the query builder will be shown for any
          * route that does not have a defined action
