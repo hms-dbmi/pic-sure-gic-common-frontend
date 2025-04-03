914a6e3e095ce83475ca6ea792fd0c6d1c26884e
Luke Sikina <lucas.skina@gmail.com>
2025-04-03 13:44:35 -0400

    [ALS-8531] Exponential backoff for errored sites
    - When a site breaks, avoid hitting it for 1, 2, 4, 8 secs.. etc
    - Mark sites as unstable after backoff period

--- pic-sure-passthru-resource/passthru/src/test/java/edu/harvard/hms/avillach/passthru/http/HttpRequestServiceTest.java
+++ pic-sure-passthru-resource/passthru/src/test/java/edu/harvard/hms/avillach/passthru/http/HttpRequestServiceTest.java
@@ -2,15 +2,15 @@ package edu.harvard.hms.avillach.passthru.http;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.harvard.hms.avillach.passthru.status.ResourceStatusService;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.StatusLine;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.protocol.HttpClientContext;
 import org.apache.http.client.HttpClient;
+import org.apache.http.conn.ConnectTimeoutException;
 import org.apache.http.entity.BasicHttpEntity;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.StringEntity;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -20,7 +20,6 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.util.Optional;
 
@@ -32,6 +31,9 @@ class HttpRequestServiceTest {
 
     @MockitoBean
     HttpClient client;
+    
+    @MockitoBean
+    ResourceStatusService statusService;
 
     @MockitoBean
     HttpClientContext context;
@@ -40,7 +42,8 @@ class HttpRequestServiceTest {
 
     @Test
     void shouldStopWeirdHeaders() {
-        Optional<Object> actual = subject.post(null, new Object(), Object.class, "I'm a headless header :(");
+        Mockito.when(statusService.isSiteDown(URI.create("foo.invalid"))).thenReturn(false);
+        Optional<Object> actual = subject.post(URI.create("foo.invalid"), "./", new Object(), Object.class, "I'm a headless header :(");
 
         Assertions.assertEquals(Optional.empty(), actual);
         Mockito.verifyNoInteractions(client);
@@ -48,6 +51,7 @@ class HttpRequestServiceTest {
 
     @Test
     void shouldHandleErrorCodes() throws IOException {
+        Mockito.when(statusService.isSiteDown(URI.create("foo.invalid"))).thenReturn(false);
         HttpResponse response = Mockito.mock(HttpResponse.class);
         StatusLine status = Mockito.mock(StatusLine.class);
         Mockito.when(status.getStatusCode()).thenReturn(500);
@@ -55,7 +59,7 @@ class HttpRequestServiceTest {
         Mockito.when(client.execute(Mockito.any(HttpPost.class), Mockito.any(HttpClientContext.class)))
             .thenReturn(response);
 
-        Optional<TestObj> actual = subject.post(null, new TestObj("Frank"), TestObj.class, "a", "1");
+        Optional<TestObj> actual = subject.post(URI.create("foo.invalid"), "./", new TestObj("Frank"), TestObj.class, "a", "1");
 
         Assertions.assertEquals(Optional.empty(), actual);
         Mockito.verify(client, Mockito.times(1))
@@ -64,6 +68,7 @@ class HttpRequestServiceTest {
 
     @Test
     void shouldPost() throws IOException {
+        Mockito.when(statusService.isSiteDown(URI.create("foo.invalid"))).thenReturn(false);
         HttpEntity entity = makeEntity(new TestObj("Bill"));
         HttpResponse response = Mockito.mock(HttpResponse.class);
         StatusLine status = Mockito.mock(StatusLine.class);
@@ -73,12 +78,79 @@ class HttpRequestServiceTest {
         Mockito.when(client.execute(Mockito.any(HttpPost.class), Mockito.any(HttpClientContext.class)))
             .thenReturn(response);
 
-        Optional<TestObj> actual = subject.post(null, new TestObj("Frank"), TestObj.class, "a", "1");
+        Optional<TestObj> actual = subject.post(URI.create("foo.invalid"), "./", new TestObj("Frank"), TestObj.class, "a", "1");
         Optional<TestObj> expected = Optional.of(new TestObj("Bill"));
 
         Assertions.assertEquals(expected, actual);
         Mockito.verify(client, Mockito.times(1))
             .execute(Mockito.any(HttpPost.class), Mockito.any(HttpClientContext.class));
+    }
+
+    @Test
+    void shouldPostForWeirdCodes() throws IOException {
+        Mockito.when(statusService.isSiteDown(URI.create("foo.invalid"))).thenReturn(false);
+        HttpEntity entity = makeEntity(new TestObj("Bill"));
+        HttpResponse response = Mockito.mock(HttpResponse.class);
+        StatusLine status = Mockito.mock(StatusLine.class);
+        Mockito.when(status.getStatusCode()).thenReturn(100);
+        Mockito.when(response.getStatusLine()).thenReturn(status);
+        Mockito.when(response.getEntity()).thenReturn(entity);
+        Mockito.when(client.execute(Mockito.any(HttpPost.class), Mockito.any(HttpClientContext.class)))
+            .thenReturn(response);
+
+        Optional<TestObj> actual = subject.post(URI.create("foo.invalid"), "./", new TestObj("Frank"), TestObj.class, "a", "1");
+        Optional<TestObj> expected = Optional.of(new TestObj("Bill"));
+
+        Assertions.assertEquals(expected, actual);
+        Mockito.verify(client, Mockito.times(1))
+            .execute(Mockito.any(HttpPost.class), Mockito.any(HttpClientContext.class));
+    }
+
+    @Test
+    void shouldNotPostIfSiteIsDown() {
+        Mockito.when(statusService.isSiteDown(URI.create("foo.invalid"))).thenReturn(true);
+        Optional<Object> actual = subject.post(URI.create("foo.invalid"), "./", new Object(), Object.class);
+
+        Assertions.assertEquals(Optional.empty(), actual);
+        Mockito.verifyNoInteractions(client);
+    }
+
+    @Test
+    void shouldMarkSiteASDownForCodes() throws IOException {
+        Mockito.when(statusService.isSiteDown(URI.create("foo.invalid"))).thenReturn(false);
+        HttpResponse response = Mockito.mock(HttpResponse.class);
+        StatusLine status = Mockito.mock(StatusLine.class);
+        Mockito.when(status.getStatusCode()).thenReturn(504); // simulate site timeout
+        Mockito.when(response.getStatusLine()).thenReturn(status);
+        Mockito.when(client.execute(Mockito.any(HttpPost.class), Mockito.any(HttpClientContext.class)))
+            .thenReturn(response);
+
+        Optional<TestObj> actual = subject.post(URI.create("foo.invalid"), "./", new TestObj("Frank"), TestObj.class, "a", "1");
+
+        Assertions.assertEquals(Optional.empty(), actual);
+        Mockito.verify(client, Mockito.times(1))
+            .execute(Mockito.any(HttpPost.class), Mockito.any(HttpClientContext.class));
+        Mockito.verify(statusService, Mockito.times(1))
+            .markAsOffline(URI.create("foo.invalid"));
+    }
+
+    @Test
+    void shouldMarkSiteASDownForException() throws IOException {
+        Mockito.when(statusService.isSiteDown(URI.create("foo.invalid"))).thenReturn(false);
+        HttpResponse response = Mockito.mock(HttpResponse.class);
+        StatusLine status = Mockito.mock(StatusLine.class);
+        Mockito.when(status.getStatusCode()).thenReturn(200); // ok http code, but throw exception
+        Mockito.when(response.getStatusLine()).thenReturn(status);
+        Mockito.when(client.execute(Mockito.any(HttpPost.class), Mockito.any(HttpClientContext.class)))
+            .thenThrow(new ConnectTimeoutException());
+
+        Optional<TestObj> actual = subject.post(URI.create("foo.invalid"), "./", new TestObj("Frank"), TestObj.class, "a", "1");
+
+        Assertions.assertEquals(Optional.empty(), actual);
+        Mockito.verify(client, Mockito.times(1))
+            .execute(Mockito.any(HttpPost.class), Mockito.any(HttpClientContext.class));
+        Mockito.verify(statusService, Mockito.times(1))
+            .markAsOffline(URI.create("foo.invalid"));
     }
 
     private HttpEntity makeEntity(TestObj obj) throws JsonProcessingException {
