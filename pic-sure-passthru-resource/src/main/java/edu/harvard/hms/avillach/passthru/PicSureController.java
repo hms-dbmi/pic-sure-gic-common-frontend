6760fadc794009abb040f413d52919f4a273d37d
James <James_Peck@hms.harvard.edu>
2025-07-29 15:18:49 -0400

    add @RequestBody to status and result

--- pic-sure-passthru-resource/src/main/java/edu/harvard/hms/avillach/passthru/PicSureController.java
+++ pic-sure-passthru-resource/src/main/java/edu/harvard/hms/avillach/passthru/PicSureController.java
@@ -49,12 +49,12 @@ public class PicSureController {
     }
 
     @PostMapping("/query/{resourceQueryId}/result")
-    public ResponseEntity<Object> queryResult(@PathVariable("resourceQueryId") String queryId, QueryRequest request) {
+    public ResponseEntity<Object> queryResult(@PathVariable("resourceQueryId") String queryId, @RequestBody QueryRequest request) {
         return formatRequestAndRunPost(request, "./query/" + queryId + "/result", Object.class);
     }
 
     @PostMapping("/query/{resourceQueryId}/status")
-    public ResponseEntity<QueryStatus> queryStatus(@PathVariable("resourceQueryId") String queryId, QueryRequest request) {
+    public ResponseEntity<QueryStatus> queryStatus(@PathVariable("resourceQueryId") String queryId, @RequestBody QueryRequest request) {
         return formatRequestAndRunPost(request, "./query/" + queryId + "/status", QueryStatus.class);
     }
 
