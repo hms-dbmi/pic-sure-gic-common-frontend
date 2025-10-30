9a7e612ef5841d20af4711e7a53ea255ba0f17c1
Samantha Piatt <samantha.piatt@childrens.harvard.edu>
2025-10-30 13:57:32 -0400

    Add remote resource to search path.

--- pic-sure-passthru-resource/src/main/java/edu/harvard/hms/avillach/passthru/PicSureController.java
+++ pic-sure-passthru-resource/src/main/java/edu/harvard/hms/avillach/passthru/PicSureController.java
@@ -66,7 +66,15 @@ public class PicSureController {
     @PostMapping("/search")
     public ResponseEntity<SearchResults> search(@RequestBody QueryRequest request) {
         String path = request == null ? "" : request.getResourceUUID().toString();
-        return formatRequestAndRunPost(request, "./picsure/search/" + path, SearchResults.class);
+
+        Optional<RemoteResource> maybeResource = remoteResourceService.getRemoteResource(request.getResourceUUID());
+        if (maybeResource.isEmpty()) {
+            log.info("Could not find remote resource with uuid of {}", request.getResourceUUID());
+            return ResponseEntity.notFound().build();
+        }
+        RemoteResource remote = maybeResource.get();
+
+        return formatRequestAndRunPost(request, "./search/" + remote.remote(), SearchResults.class);
     }
 
     @PostMapping("/query/format")
